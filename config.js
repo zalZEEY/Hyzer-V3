8a2776ca9d7db786a879712804bbd4123ac414c9
Z E E Y <88220322+zalZEEY@users.noreply.github.com>
2022-03-25 05:44:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.linkGC = ['https://chat.whatsapp.com/Jzd9DEVB5nODtNBk1VCNrV', 'https://chat.whatsapp.com/Jzd9DEVB5nODtNBk1VCNrV', 'https://chat.whatsapp.com/HBhy7rVae3o0PkIfkhvs2N'] // ganti jadi group lu
+global.linkGC = ['https://chat.whatsapp.com/Gagjs8K5xJo2qnidIwAB3u', 'https://chat.whatsapp.com/Gagjs8K5xJo2qnidIwAB3u', 'https://chat.whatsapp.com/Gagjs8K5xJo2qnidIwAB3u'] // ganti jadi group lu
 global.owner = ['62895395165760', '62895395165760', '62895395165760'] // Masukan nomot kalian
 global.mods = ['6287892711054', '6287892711054'] // Moderator
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
@@ -55,7 +55,7 @@ const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
   var sticker_name = 'hehe'
-  var sticker_author = 'by ShiroBotz'
+  var sticker_author = 'by Zeey'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
